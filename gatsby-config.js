13b0269fecd6bab26948fbbca4389128d94013b3
nabhoneel <nabhoneel.95@gmail.com>
2019-01-24 11:22:15 +0530

    removed offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    'gatsby-plugin-offline',
     'gatsby-plugin-sass',
     {
       resolve: "gatsby-source-wordpress",
